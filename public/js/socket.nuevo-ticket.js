59c350d67b7dbd337eb7313892ce59b78ea00462
Ariadna <ariadna.marull@gmail.com>
2020-03-02 14:41:50 -0300

    Primer commit -Produccion

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,31 @@
+var socket = io();
+
+var label = $('#lblNuevoTicket');
+
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('Desconectado del servidor');
+});
+
+// on 'estadoActual'
+socket.on('estadoActual', function(resp) {
+
+    console.log(resp);
+    label.text(resp.actual);
+
+});
+
+//Listener Query
+$('button').on('click', function() {
+
+    socket.emit('siguienteTicket', null, function(siguienteTicket) {
+
+        label.text(siguienteTicket);
+
+    });
+
+});
